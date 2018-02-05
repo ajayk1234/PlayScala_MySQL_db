2de9a8e2fbf8b4957050d27d36b03786cdfaed1b
ajayk1234 <ajaykangare1993@gmail.com>
2018-02-05 12:41:52 +0530

    login form with MySQL DB

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -3,49 +3,66 @@ import javax.inject.Inject
 
 import play.api.db._
 import play.api.mvc._
-import scala.collection.mutable.ListBuffer
-import javax.inject._
 import play.api.data.Form
 import play.api.data.Forms.{mapping, nonEmptyText}
 import play.api.i18n.I18nSupport
-import views.html.helper.form
+import sun.security.util.Password
 
-case class User(firstname: String, lastname: String)
+case class User(username: String, password: String)
+case class UserData(firstname : String, lastname: String, email:String,username:String,password: String )
 
 class HomeController @Inject()(db: Database,
                                val controllerComponents: ControllerComponents)
     extends BaseController
     with I18nSupport {
+//  mapping of userdetails class
+
+  val userDataForm: Form[UserData] = Form(
+    mapping(
+      "firstname" -> nonEmptyText,
+      "lastname" -> nonEmptyText,
+      "email" -> nonEmptyText,
+      "username" -> nonEmptyText,
+      "password" -> nonEmptyText
+    )(UserData.apply)(UserData.unapply)
+  )
 
 //  mapping of user class
   val userForm: Form[User] = Form(
     mapping(
-      "firstname" -> nonEmptyText,
-      "lastname" -> nonEmptyText
+      "username" -> nonEmptyText,
+      "password" -> nonEmptyText
     )(User.apply)(User.unapply)
   )
 
+  def home = Action {
+    Ok(views.html.home())
+  }
+
 //  render user form which take input as firstname and lastname
   def addUser = Action { implicit request =>
-    Ok(views.html.user(userForm))
+    Ok(views.html.user(userDataForm))
 
   }
 
 //  add values of User object to user table and shows message
   def addUserToDB = Action { implicit request =>
-    val user = userForm.bindFromRequest.get //get user object which has input values
+    val user = userDataForm.bindFromRequest.get //get user object which has input values
     val conn = db.getConnection() // conn object create connection which MySQL DB
 
     try {
-      val prestmt = conn.prepareStatement("insert into user values(?,?)")
+      val prestmt = conn.prepareStatement("insert into user values(?,?,?,?,?)")
       prestmt.setString(1, user.firstname)
       prestmt.setString(2, user.lastname)
+      prestmt.setString(3, user.email)
+      prestmt.setString(4, user.username)
+      prestmt.setString(5, user.password)
       prestmt.executeUpdate()
     } finally {
       conn.close()
     }
 
-    Ok("User added successfully")
+    Ok("User Added Successfully")
   }
 
 //  fetch all the users from database and store it in map and call user html page which show all users
@@ -58,14 +75,45 @@ class HomeController @Inject()(db: Database,
       val stmt = conn.createStatement
       val rs = stmt.executeQuery("SELECT * from user ")
       while (rs.next()) {
-        var firstname = rs.getString("firstname")
-        var lastname = rs.getString("lastname")
+        var firstname = rs.getString("username")
+        var lastname = rs.getString("password")
         users += firstname -> lastname
       }
     } finally {
       conn.close()
     }
-    Ok(views.html.user(users))
+    Ok(views.html.index(users))
   }
+
+  def login = Action{ implicit request =>
+    Ok(views.html.login(userForm))
+  }
+
+  def doLogin = Action{ implicit request =>
+    val user = userForm.bindFromRequest.get //get user object which has input values
+    val conn = db.getConnection() // conn object create connection which MySQL DB
+    var msg = ""
+
+    try {
+      val prestmt = conn.prepareStatement("select * from user where username = ? and password = ?")
+      prestmt.setString(1, user.username)
+      prestmt.setString(2, user.password)
+      val rs = prestmt.executeQuery()
+      if(rs.next()) {
+        var firstname = rs.getString("username")
+        var lastname = rs.getString("password")
+        msg = "Login successfull"
+      }
+      else{
+        msg= "Unauthorized"
+      }
+
+    } finally {
+      conn.close()
+    }
+
+    Ok(msg)
+  }
+
 
 }
